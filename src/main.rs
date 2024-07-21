ca430336e5ea2e6b327b7918061e6c045b21c674
jiangshu <jiangshu@lightpaw.com>
2024-07-21 18:00:16 +0800

    feature: build api skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use chat::{get_router, AppConfig};
+use tokio::net::TcpListener;
+use tracing::info;
+use tracing_subscriber::{
+    filter::LevelFilter, fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _,
+};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let layer = Layer::new().pretty().with_filter(LevelFilter::INFO);
+    tracing_subscriber::registry().with(layer).init();
+
+    let config = AppConfig::load()?;
+    let addr = format!("0.0.0.0:{}", config.server.port);
+
+    let app = get_router(config);
+    let listener = TcpListener::bind(&addr).await?;
+
+    axum::serve(listener, app.into_make_service()).await?;
+    info!("Listening on: {}", addr);
+
+    Ok(())
 }
