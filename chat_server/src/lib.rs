e6e6ecfb15f12eada4708ae6a0f71ae29288b849
luffy <bestparty2025@gmail.com>
2024-08-06 13:53:35 +0800

    fix: test for github workflow action

--- chat_server/src/lib.rs
+++ chat_server/src/lib.rs
@@ -105,9 +105,11 @@ mod test_util {
         pub async fn new_for_test(config: AppConfig) -> Result<(TestPg, Self), AppError> {
             let dk = DecodingKey::load(&config.auth.pk).context("load pk failed")?;
             let ek = EncodingKey::load(&config.auth.sk).context("load ek failed")?;
-            let post = config.server.db_url.rfind('/').expect("invalid db_url");
-            let server_url = &config.server.db_url[..post];
-            let (tdb, pool) = get_test_pool(Some(server_url)).await;
+            // let post = config.server.db_url.rfind('/').expect("invalid db_url");
+            // let server_url = &config.server.db_url[..post];
+            // let server_url = "postgres://postgres:postgres@localhost:5432";
+            // let (tdb, pool) = get_test_pool(Some(server_url)).await;
+            let (tdb, pool) = get_test_pool(None).await;
             let state = Self {
                 inner: Arc::new(AppStateInner {
                     config,
@@ -121,7 +123,7 @@ mod test_util {
     }
 
     pub async fn get_test_pool(url: Option<&str>) -> (TestPg, PgPool) {
-        let url = url.unwrap_or("postgres://postgres:password@localhost:5432");
+        let url = url.unwrap_or("postgres://postgres:postgres@localhost:5432");
         let tdb = TestPg::new(url.to_string(), std::path::Path::new("../migrations"));
         let pool = tdb.get_pool().await;
 
